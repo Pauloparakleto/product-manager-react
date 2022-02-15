0d7769c80df1bb55751d2ec104099d2433b606ef
paulo-felipe <alparakleto@gmail.com>
2022-02-15 19:20:30 -0300

    feat(product.js): add grid paper and delet button

--- src/products/product.js
+++ src/products/product.js
@@ -1,7 +1,34 @@
 import React, {Component} from "react";
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyle = makeStyles({
+    root: {
+        height: "3em",
+        padding: "2em",
+        margin: "1em",
+    }
+})
 
 export default function Product(props){
+    const classes = useStyle();
     return (
-        <li>{props.item.name}</li>
+        <Grid container spacing={0}>
+            <Grid item xs={9}>
+                <Paper elevation={3} className={classes.root}>
+                    {props.item.name}
+                    <hr/>
+                    <Button variant="contained" color="secondary">
+                        Delete
+                        <DeleteIcon className={classes.rightIcon} />
+                    </Button>
+                </Paper>
+            </Grid>
+            
+        </Grid>
+        
     )
 }
